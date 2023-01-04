e74686eb4469b01bfc985043bc043519ea7487cb
Akintunde Oladipo <akin.o.oladipo@gmail.com>
2023-01-03 22:06:11 -0500

    minor fixes

--- scripts/encode_and_search.sh
+++ scripts/encode_and_search.sh
@@ -11,31 +11,31 @@ export CUDA_VISIBLE_DEVICES=2
 
 # Query Encoding
 python -m tevatron.driver.encode \
---output_dir=/store2/scratch/aooladip/projects/tevatron/outputs/davlan-swahili-bert/encoded_queries \
+--output_dir=outputs/davlan-swahili-bert/encoded_queries \
 --model_name_or_path models/davlan-swahili-bert \
 --fp16 \
 --per_device_eval_batch_size 256 \
 --dataset_name castorini/mr-tydi:swahili/test \
---encoded_save_path /store2/scratch/aooladip/projects/tevatron/outputs/davlan-swahili-bert/query_embedding/swahili/query_emb.pkl \
+--encoded_save_path outputs/davlan-swahili-bert/query_embedding/swahili/query_emb.pkl \
 --q_max_len 64 \
 --encode_is_qry && enc_query=true
 
 # Corpus Encoding
 python -m tevatron.driver.encode \
---output_dir=/store2/scratch/aooladip/projects/tevatron/outputs/davlan-swahili-bert/encoded_corpus \
+--output_dir=outputs/davlan-swahili-bert/encoded_corpus \
 --model_name_or_path models/davlan-swahili-bert \
 --fp16 \
 --per_device_eval_batch_size 256 \
 --p_max_len 256 \
 --dataset_name castorini/mr-tydi-corpus:swahili \
---encoded_save_path /store2/scratch/aooladip/projects/tevatron/outputs/davlan-swahili-bert/corpus_encoding/swahili/corpus_emb.pkl \
+--encoded_save_path outputs/davlan-swahili-bert/corpus_encoding/swahili/corpus_emb.pkl \
 --encode_num_shard 1 && enc_corpus=true
 
 # Search
 if [[ $enc_corpus && $enc_query ]]; then
     python -m tevatron.faiss_retriever \
-    --query_reps /store2/scratch/aooladip/projects/tevatron/outputs/davlan-swahili-bert/query_embedding/swahili/query_emb.pkl \
-    --passage_reps /store2/scratch/aooladip/projects/tevatron/outputs/davlan-swahili-bert/corpus_encoding/swahili/corpus_emb.pkl \
+    --query_reps outputs/davlan-swahili-bert/query_embedding/swahili/query_emb.pkl \
+    --passage_reps outputs/davlan-swahili-bert/corpus_encoding/swahili/corpus_emb.pkl \
     --depth 100 \
     --batch_size -1 \
     --save_text \
